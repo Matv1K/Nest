205a17dcab2114ac2ef6edb46d9f051a99615b00
Matv1K <balahonovmatvej@gmail.com>
2025-06-22 14:15:54 +0300

    Add TypeORM, rewrite logic accordingly and fix configuration issues

--- src/norsemen/norsemen.module.ts
+++ src/norsemen/norsemen.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { NorsemenController } from "./norsemen.controller";
 import { NorsemenService } from "./norsemen.service";
 
+import { Norseman } from "./norseman.entity";
+
 @Module({
+    imports: [TypeOrmModule.forFeature([Norseman])],
     controllers: [NorsemenController],
     providers: [NorsemenService],
 })
